314603f6a0f888232b2a3993bf2a89721e615834
AlexSeverin <alex.nixie@mail.ru>
2019-10-22 20:48:20 +0300

    pandas and plot

--- voting.py
+++ voting.py
@@ -27,15 +27,46 @@ for tr in table_rows:
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
+URL = 'http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&root=1&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=0&prver=0&pronetvd=null&vibid=27820001217417&type=222'
 
-dfs = pd.read_html('http://www.st-petersburg.vybory.izbirkom.ru/region/region/st-petersburg?action=show&tvd=27820001217417&vrn=27820001217413&region=78&global=&sub_region=78&prver=0&pronetvd=null&vibid=27820001217430&type=222',header=0,encoding='cp1251')
-for df in dfs:
+df1 = pd.read_html(URL,header=0,encoding='cp1251')[5].iloc[:, 1:].rename(columns={'Unnamed: 1':'Name'}) #Наименоване избирательной комиссии
+print(df1)
+
+df2 = pd.read_html(URL,encoding='cp1251')[6].iloc[:, 1:] #Территориальные избирательные комиссии с 1 по 30
+print(df2)
+
+
+#print(df2.loc[13])
+#sudo apt-get install python3-tk
+
+X = (df2.loc[1]).astype(int)
+x=[]
+for i in X.index:
+    c = X.index.get_value(X,i)
+    x.append(c)
+print(x)
+
+Y = (df2.loc[3]).astype(int)
+y =[]
+for i in Y.index:
+    l = Y.index.get_value(Y,i)
+    y.append(l)
+print(y)
+
+plt.plot(x,y,'ko')
+plt.show()
+
+#new_sample_df = df2.loc[1]
+#new_sample_df.plot()
+#plt.show()
+
+#for df in dfs:
 #    print(df)
 #    df.reset_index(inplace=True)
-    print(df.head())
+ #   print(df.head())
 
 style.use('fivethirtyeight')
 
-df['УИК №1725'].plot()
-plt.legend()
-plt.show()
+#df['УИК №1725'].plot()
+#plt.legend()
+#plt.show()
